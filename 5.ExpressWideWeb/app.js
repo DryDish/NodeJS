265f8dbf689a708447ff61fe2ae966c46af20135
alex <57399961+IroniX2@users.noreply.github.com>
2021-03-11 12:06:18 +0100

    add crud to expresswideweb and add redirect buttons

--- 5.ExpressWideWeb/app.js
+++ 5.ExpressWideWeb/app.js
@@ -1,23 +1,82 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const htmlFolder = __dirname + "/public/";
 
+let id = 3;
+
+let plants = [
+  {
+    id: 1,
+    title: "fire",
+  },
+  {
+    id: 2,
+    title: "ice",
+  },
+];
+
+
+
 app.get("/", (req, res) => {
-    res.sendFile(htmlFolder + "welcome.html");
-})
+  res.sendFile(htmlFolder + "welcome.html");
+});
 
 app.get("/dragons", (req, res) => {
-    res.sendFile(htmlFolder + "dragons.html");
+  res.sendFile(htmlFolder + "dragons.html");
+});
+
+app.get("/safeport", (req, res) => {
+  res.send({ message: "you are safe here"})
+})
+
+app.get("/dangerport", (req, res) => {
+  res.redirect("/safeport")
+})
+
+app.get("/plants", (req, res) => {
+    res.send(plants);
+  });
+
+app.post("/plants", (req, res) => {
+  const newPlant = req.body;
+  // add the id
+  newPlant.id = id++;
+  // send the data back
+  res.send({ newPlant });
+  // add to array:
+  plants.push(newPlant)
+});
+
+app.patch("plants/:id", (req, res) => {
+  let updated = false;
+  plants = plants.map(plant => {
+    if (plant.id === Number(req.params.id))
+    {
+      updated = true;
+      return {...plant, ...req.body, id: plant.id};
+    }
+    return plant;
+  })
+
+  res.send({updated : updated});
+})
+
+app.delete("/plants/:id", (req, res) => {
+  const delete_id = Number(req.params.id);
+
+  plants = plants.filter(plant => plant.id != id)
+
+  res.send({});
 })
 
 const PORT = process.env.PORT || 8080;
-console.log(PORT)
-app.listen(8080, (error) => 
-{
-    if (error)
-    {
-        console.log(error);
-    }
-    console.log("Server is running on port:", PORT);
+console.log(PORT);
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log("Server is running on port:", PORT);
 });
