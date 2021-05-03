e00e7540ffff644cbf9f4e10b16bfd90db2661a1
alex <57399961+IroniX2@users.noreply.github.com>
2021-05-03 22:01:09 +0200

    add finalized projects.js and projects.css

--- Nodefolio/public/projects/projects.js
+++ Nodefolio/public/projects/projects.js
@@ -1,26 +1,71 @@
 // fetch("/api/projects").then(res => res.json()).then(console.log);
 
 (async function getProjects() {
-    const response = await fetch("/api/projects");
-    const result = await response.json();
+  const fetchInfo = await fetch("/api/projects");
+  const data = await fetchInfo.json();
 
-    const projectsDiv = document.getElementById("projects");
+  const divOfProjects = document.getElementById("projects");
 
-    result.projects.map(project => {
-        const projectDiv = document.createElement("div");
+  data.projects.map((project) => {
+    // Create the div
+    const projectDiv = document.createElement("div");
 
-        const projectTitle = document.createElement("h2");
-        projectTitle.classList.add("project-title");
-        projectTitle.innerText = project.title;
+    // Create the title header and insert it from list
+    const projectTitle = document.createElement("h2");
+    projectTitle.classList.add("project-title");
+    projectTitle.innerText = project.title;
 
-        const projectDescription = document.createElement("p");
-        projectDescription.classList.add("project-description");
-        projectDescription.innerText = project.description;
+    // Create the description paragraph and populate it from list
+    const projectDesc = document.createElement("p");
+    projectDesc.classList.add("project-description");
+    projectDesc.innerText = project.description;
 
-        projectDiv.appendChild(projectTitle);
-        projectDiv.appendChild(projectDescription);
-        projectsDiv.appendChild(projectDiv);
-    });
+    
+    // Create a list and populate it with technologies
+    const projectTech = document.createElement("p");
+    projectTech.classList.add("project-technologies");
+    projectTech.innerText = "Technologies used:";
+
+    // Take all the elements of the list and place them into a list
+    let listOfTechs = [];
+    listOfTechs =  project.technologies.map(tech => {
+        const projectTechnologies = document.createElement("span");
+        projectTechnologies.classList.add("technology");
+        console.log(tech);
+        projectTechnologies.innerText = tech;
+        return projectTechnologies;
+    })
+
+    // Append the list elements one at a time to project tech
+    for(i = 0 ; i < listOfTechs.length; i++)
+    {
+        projectTech.appendChild(listOfTechs[i]);
+    }
+
+    
+    // GitHub line Prefix
+    const gitPrefix = document.createElement("span");
+    gitPrefix.innerText = "GitHub link: ";
+    // GitHub line URL
+    const gitUrl = document.createElement("a");
+    gitUrl.classList.add("url-link");
+    gitUrl.href = project.gitURL;
+    gitUrl.innerText = project.gitURL;
+
+    const separator = document.createElement("hr");
+
+    
+
+    // Append all the elements to the main div
+    projectDiv.appendChild(projectTitle);
+    projectDiv.appendChild(projectDesc);
+    projectDiv.appendChild(projectTech);
+
+    // GitHub line
+    projectDiv.appendChild(gitPrefix);
+    projectDiv.appendChild(gitUrl);
+    projectDiv.appendChild(separator);
+    // Append main div to the div in the HTML
+    divOfProjects.appendChild(projectDiv);
+  });
 })();
-
-
--- Nodefolio/routes/projects.js
+++ Nodefolio/routes/projects.js
@@ -1,19 +1,31 @@
 const router = require("express").Router();
 
 const projects = [
-    {
-        title: "Nodefolio",
-        description: "Created a personal portfolio with Node.js",
-        gitURL: "https://github.com/IroniX2/NodeJS/tree/main/Nodefolio",
-        technologiesInvolved: ["Node.js", "Html", "CSS"]
-    }
+  {
+    title: "Nodefolio",
+    description: "Created a personal portfolio with Node.js",
+    gitURL: "https://github.com/IroniX2/NodeJS/tree/main/Nodefolio",
+    technologies: ["Node.js", "Html", "CSS"],
+  },
+  {
+    title: "Another supercool project",
+    description:
+      "A project of great importance, and an equally important description",
+    gitURL: "https://github.com/IroniX2/404",
+    technologies: ["Java", "MySQL"],
+  },
+  {
+    title: "Keyboard OS is Not Real!?",
+    description: "Well at least we got a description that says something other than nothing",
+    gitURL: "https://github.com/IroniX2/404",
+    technologies: ["Probably some tech", "And some other tech"],
+  },
 ];
 
 router.get("/api/projects", (req, res) => {
-    res.send({ projects });
+  res.send({ projects });
 });
 
-
 module.exports = {
-    router
+  router,
 };
